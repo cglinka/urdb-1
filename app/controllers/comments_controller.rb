4418e0cd1a51ac2f5a1b83365a559eff264a7638
Clare Glinka <glinka.cb@gmail.com>
2013-10-07 16:21:45 -0500

    Add comments controller with create action.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @comment = @movie.comments.new(comment_params)
+
+    if @comment.save
+      redirect_to @comment.movie, notice: 'Comment was successfully created.'
+    else
+      redirect_to @comment.movie, alert: 'Comment was not successfully created.'
+    end
+  end
+
+  private
+
+  def comment_params
+    params[:comment].permit(:user, :comment)
+  end
+end
