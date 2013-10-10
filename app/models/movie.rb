b86759b2f34b89fbfb760e6490cb9ef6ce21a3e3
Clare Glinka <glinka.cb@gmail.com>
2013-10-10 10:36:34 -0500

    Add validation to Movie class model.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,8 @@ class Movie < ActiveRecord::Base
   has_many :roles
   has_many :stars, through: :roles
 
+  validates :title, presence: true
+
   def rotten_finder
     RottenMovie.find(title: title, limit: 1)
   end
